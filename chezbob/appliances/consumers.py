2e8429a11d5d119fdcd8457ace5807d17c340f4e
Ken Hoover <k.hoover3@gmail.com>
2019-11-06 10:54:54 -0800

    Automatically encode/decode websock messages to Message objects.

--- chezbob/appliances/consumers.py
+++ chezbob/appliances/consumers.py
@@ -10,7 +10,7 @@ from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer, JsonWebsocketConsumer
 from django.utils import timezone
 
-from .protocol.messages import MESSAGE_TYPES, MessageHeader, PingMessage, PongMessage
+from .protocol.messages import MessageEncoder, MessageDecoder, PingMessage, PongMessage
 from .models import Appliance
 
 # Get an instance of a logger
@@ -20,10 +20,19 @@ logger = logging.getLogger(__name__)
 class ApplianceConsumer(JsonWebsocketConsumer, metaclass=ABCMeta):
     appliance_uuid: str
 
+    @classmethod
+    def encode_json(cls, content):
+        return MessageEncoder.encode(content)
+
+    @classmethod
+    def decode_json(cls, text_data):
+        return MessageDecoder.decode(text_data)
+
     def __init__(self, scope):
         super().__init__(scope)
         kwargs = scope['url_route']['kwargs']
         self.appliance_uuid = kwargs['appliance_uuid']
+
 
     def connect(self):
         logger.info(f"[{self.appliance_uuid}] Connecting...")
@@ -36,16 +45,7 @@ class ApplianceConsumer(JsonWebsocketConsumer, metaclass=ABCMeta):
         super().disconnect(code)
         self.status_down()
 
-    def receive_json(self, content, **kwargs):
-        header_content = content.pop('header')
-        header = MessageHeader(**header_content)
-
-        klass = MESSAGE_TYPES[header.msg_type]
-        msg = klass(header=header, **content)
-
-        return self.receive_message(msg, **kwargs)
-
-    def receive_message(self, msg, **_kwargs):
+    def receive_json(self, msg, **kwargs):
         if isinstance(msg, PingMessage):
             self.receive_ping(msg)
 
@@ -54,7 +54,7 @@ class ApplianceConsumer(JsonWebsocketConsumer, metaclass=ABCMeta):
 
     def send_pong(self, content: str):
         pong_msg = PongMessage(pong=content)
-        self.send_json(pong_msg.to_json())
+        self.send_json(pong_msg)
 
     # Database Actions
     # ----------------
