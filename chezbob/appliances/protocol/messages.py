816c7a5fec102427fc9229f6c960a9fef3e24315
Ken Hoover <k.hoover3@gmail.com>
2019-11-06 10:54:02 -0800

    Added a custom JSONEncoder and JSONDecoder for messages.
    Also automatically handles the case of submessages, i.e. messages that
    contain other messages.

--- chezbob/appliances/protocol/messages.py
+++ chezbob/appliances/protocol/messages.py
@@ -1,6 +1,7 @@
+import json
+
 from bidict import bidict
 from django.conf import settings
-
 
 class MessageHeader:
     __slots__ = ['msg_type', 'version']
@@ -13,8 +14,11 @@ class MessageHeader:
         if version is None:
             self.version = settings.BOBOLITH_PROTOCOL_VERSION
 
+    def to_json(self):
+        return {'msg_type': self.msg_type, 'version': self.version}
 
-MESSAGE_TYPES = bidict()
+
+MESSAGE_TYPES = bidict({'header': MessageHeader})
 
 
 def message_mixin(msg_type: str):
@@ -52,3 +56,30 @@ class PongMessage(message_mixin('pong')):
     __slots__ = ['pong']
 
     pong: str
+
+def _encode(o):
+    if o.__class__ in MESSAGE_TYPES.inverse:
+        return o.to_json()
+    else:
+        raise TypeError(f'Object of type {o.__class__.__name__} '
+                        f'is not JSON serializable')
+
+def _decode(json_dict):
+    if 'header' not in json_dict:
+        return json_dict
+    content = dict(json_dict)
+    header_content = content.pop('header')
+    if type(header_content) is not dict or \
+       'msg_type' not in header_content or \
+       header_content['msg_type'] not in MESSAGE_TYPES:
+        # This is a non-message dict, just return it.
+        return json_dict
+    header = MessageHeader(**header_content)
+
+    klass = MESSAGE_TYPES[header.msg_type]
+    msg = klass(header=header, **content)
+
+    return msg
+
+MessageEncoder = json.JSONEncoder(default=_encode)
+MessageDecoder = json.JSONDecoder(object_hook=_decode)
